304e60c4e20bf9c1017d4245bab90eb29b085872
2eux <izuruna@gmail.com>
2026-07-15 15:03:46 +0700

    Fix koneksi.php with correct DB host + auto-init tables

--- config/koneksi.php
+++ config/koneksi.php
@@ -7,11 +7,24 @@ $database = "mdm_portal";
 
 $koneksi = new mysqli($host, $username, $password, $database);
 
-
 if ($koneksi->connect_error) {
      error_log("Koneksi gagal: " . $koneksi->connect_error);
     die("Maaf, terjadi masalah pada sistem.");
 }
 
 $koneksi->set_charset("utf8mb4");
-?>
+
+// Auto-init database tables on first load
+$init_flag = '/tmp/db_initialized.flag';
+if (!file_exists($init_flag)) {
+    $check = $koneksi->query("SHOW TABLES LIKE 'users'");
+    if ($check && $check->num_rows == 0) {
+        $sql = file_get_contents(__DIR__ . '/mdm_portal.sql');
+        $sql = preg_replace('/^CREATE DATABASE.*$/m', '', $sql);
+        $sql = preg_replace('/^USE .*$/m', '', $sql);
+        if ($koneksi->multi_query($sql)) {
+            do {} while ($koneksi->next_result());
+        }
+    }
+    file_put_contents($init_flag, 'done');
+}
